af652064b507b4735b8f57a8588060d7b1991c21
Smarika-tech <smarikapokhrel96@gmail.com>
2026-04-13 14:37:33 +0545

    feat(auth):add from validation and error handling

---
+++ src/utils/validate.js
@@ -0,0 +1,11 @@
+export const checkValidData=(email,password,name)=>{
+
+const isEmailValid= /^([a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/.test(email)
+const isPswValid= /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$/.test(password)
+const isNameValid= /^[a-zA-Z]+ [a-zA-Z]+$/.test(name)
+
+if(!isEmailValid) return "Email ID is not valid"
+if(!isPswValid) return "Enter a Strong Password of length greater than 7"
+if(!isNameValid) return "Name is not Valid"
+return null
+}
