4079c3e32212802812ae19705eb756f0fed9dd1f
Smarika-tech <smarikapokhrel96@gmail.com>
2026-04-13 13:08:18 +0545

    feat(auth): create authentication form UI

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useState } from "react";
+import Header from "./Header";
+import { BG_URL } from "../utils/constant";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const [isSignInForm, SetisSignInForm] = useState(true);
 
-export default Login
+  const toggleSignInForm = () => {
+    SetisSignInForm(!isSignInForm);
+  };
+
+  return (
+    <div>
+      <Header />
+      <div className="absolute">
+        <img src={BG_URL}/>
+      </div>
+      <form className="absolute text-white p-12 bg-[#18181B] w-3/12 my-36 mx-auto right-0 left-0 bg-opacity-85">
+        <h1 className="font-bold text-3xl py-3 text-center">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+          type="text"
+          placeholder="Full Name"
+          className="p-2 my-2 w-full bg-[#3b3b68] "
+        />)}
+        <input
+          type="text"
+          placeholder="Email Address"
+          className="p-2 my-2 w-full bg-[#3b3b68] "
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-2 my-2 w-full bg-[#3b3b68]"
+        />
+        <button className="p-3 my-6 bg-[#7C3AED] w-full hover:bg-[#A78BFA] rounded-lg">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-2">
+          {isSignInForm ? "New to Cineverse?" : "Already Registered?"}{" "}
+          <span
+            className="cursor-pointer text-[#7C3AED] font-semibold hover:text-[#A78BFA]"
+            onClick={toggleSignInForm}
+          >
+            {isSignInForm ? "Sign Up" : "Sign In"}
+          </span>{" "}
+          Now
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/utils/constant.js
@@ -0,0 +1,1 @@
+export const BG_URL ="https://assets.nflxext.com/ffe/siteui/vlv3/6a77b075-d304-4342-a055-c9e435c98b6f/web/NP-en-20260406-TRIFECTA-perspective_16d9d85b-77b5-4478-987b-91e30cd0ebd0_large.jpg"
