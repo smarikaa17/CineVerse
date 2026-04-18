340b0c202765342b49118af829cf25e3ee396e89
Smarika-tech <smarikapokhrel96@gmail.com>
2026-04-18 22:11:57 +0545

    feat(auth) : Configure firebase in login and signup formwith validation and store

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,32 +2,111 @@ import React, { useRef, useState } from "react";
 import Header from "./Header";
 import { BG_URL } from "../utils/constant";
 import { checkValidData } from "../utils/validate";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
+import { auth } from "../utils/firebase";
+import { useNavigate } from "react-router-dom";
+import { updateProfile } from "firebase/auth";
+import { useDispatch } from "react-redux";
+import { addUser } from "../store/userSlice";
+
 
 const Login = () => {
+
+
+  //route
+  const navigate= useNavigate()
+
+  //store
+  const dispatch=useDispatch();
+
+  //local states
   const [isSignInForm, SetisSignInForm] = useState(true);
-  const [errorMsg, SeterrorMsg] = useState(null)
+  const [errorMsg, SeterrorMsg] = useState(null);
 
-  const email= useRef(null)
-  const password= useRef(null)
-  const name= useRef(null)
+  //form fields
+  const email = useRef(null);
+  const password = useRef(null);
+  const name = useRef(null)
 
-  const handleButtonClick=()=>{
-     const message= checkValidData(email.current.value,password.current.value,name.current.value)
-     SeterrorMsg(message)
-  }
+  //sign in/up action
+  const handleButtonClick = () => {
+    const message = checkValidData(email.current.value, password.current.value);
+    SeterrorMsg(message);
+
+    if (message) return;
+    //else write signin and signup logic
+
+    if (!isSignInForm) {
+      //signUp logic
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value,
+        name.current.value
+      )
+        .then((userCredential) => {
+          const user = userCredential.user;
+      updateProfile(user, {
+           displayName: name.current.value
+        })
+        .then(() => {
+          // Profile updated!
+          const {uid,email,displayName}=auth.currentUser;
+          dispatch(addUser({uid, email,displayName}));
+          navigate('/browse')
+        })
+        .catch((error) => {
+          // An error occurred
+          SeterrorMsg(error.message)
+        });
+          
+         
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          SeterrorMsg(errorCode + "-" + errorMessage);
+        });
+    } else {
+      //signin logic
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value,
+         
+      )
+        .then((userCredential) => {
+          const user = userCredential.user;
+          const {uid,email,displayName}=user;
+          dispatch(addUser({uid, email,displayName}));
+          navigate('/browse')
+          console.log("This is user obj" ,user.uid, user.email, user.displayName);
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          SeterrorMsg(errorCode + "-"+ errorMessage);
+        });
+    }
+  };
 
   const toggleSignInForm = () => {
     SetisSignInForm((prev) => !prev);
   };
 
   return (
-    <div>
+    <div className="">
       <Header />
-      <div className="absolute">
+      <div className="absolute bg-gradient-to-b from-black ">
         <img src={BG_URL} />
       </div>
-      <form onSubmit={(e)=>e.preventDefault()} 
-       className="absolute text-white p-12 bg-[#18181B] w-3/12 my-36 mx-auto right-0 left-0 bg-opacity-85">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="absolute text-white p-12 bg-[#18181B] w-3/12 my-36 mx-auto right-0 left-0 bg-opacity-85"
+      >
         <h1 className="font-bold text-3xl py-3 text-center">
           {isSignInForm ? "Sign In" : "Sign Up"}
         </h1>
@@ -41,22 +120,27 @@ const Login = () => {
           />
         )}
         <input
-        ref={email}
+          ref={email}
           type="text"
           placeholder="Email Address"
           className="p-2 my-2 w-full bg-[#3b3b68]
           focus:outline-none focus:shadow-[0_0_7px_rgba(124,58,237,0.8)]"
         />
         <input
-        ref={password}
+          ref={password}
           type="password"
           placeholder="Password"
           className="p-2 my-2 w-full bg-[#3b3b68]
           focus:outline-none focus:shadow-[0_0_7px_rgba(124,58,237,0.8)]"
         />
-        <p className="text-red-500 font-semibold text-sm text-center">{errorMsg}</p>
+        <p className="text-red-500 font-semibold text-sm text-center">
+          {errorMsg}
+        </p>
 
-        <button className="p-3 my-6 bg-[#7C3AED] w-full hover:bg-[#A78BFA] rounded-lg " onClick={handleButtonClick}>
+        <button
+          className="p-3 my-6 bg-[#7C3AED] w-full hover:bg-[#A78BFA] rounded-lg "
+          onClick={handleButtonClick}
+        >
           {isSignInForm ? "Sign In" : "Sign Up"}
         </button>
         <p className="py-2">
