731dfc089427919550c277842502b84e650438ae
Smarika-tech <smarikapokhrel96@gmail.com>
2026-04-19 13:32:26 +0545

    refactor(auth): relocate auth state listener and simplify Body,App components

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,31 +1,9 @@
 import React from 'react'
-import { onAuthStateChanged } from 'firebase/auth'
-import { auth } from '../utils/firebase'
-import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { addUser, removeUser } from '../store/userSlice'
+
 
 const Body = () => { 
 
-  const dispatch = useDispatch();
-    useEffect(() => {
-      
-      const unsubscribe =  onAuthStateChanged(auth, (user) => {
-    if (user) {
-      // User is signed in
-      console.log("user:", user)
-      const {uid,email,displayName} = user;
-      dispatch(addUser({uid, email, displayName}));
-
-    } else {
-      // User is signed out
-      dispatch(removeUser())
-     
-    }
     
-  });
-   return () => unsubscribe(); 
-    }, [])
     
   
   return (
