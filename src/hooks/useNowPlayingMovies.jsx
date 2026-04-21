e895c5148f20ef44ef8f0ab851f43c1ca3ef08c4
Smarika-tech <smarikapokhrel96@gmail.com>
2026-04-21 09:31:29 +0545

    refactor:add comments

--- src/hooks/useNowPlayingMovies.jsx
+++ src/hooks/useNowPlayingMovies.jsx
@@ -1,23 +1,31 @@
-import React, { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
-import { addNowPlayingMovie } from '../store/movieSlice'
-import {API_OPTIONS} from "../utils/constant"
+import { useDispatch } from "react-redux"
+import {addNowPlayingMovie} from "./../store/movieSlice"
+import { useEffect } from "react";
+import {API_OPTIONS} from "./../utils/constant"
 
-const useNowPlayingMovies = () => {
-    //fetch data from TMDB API and update store
-   const dispatch= useDispatch()
 
+const useNowPlayingMovies=()=>{  
+   
+   //store dispatch
+ const dispatch= useDispatch()
+
+ //first define the method that calls the api and store the data came from api to store slice
   const getNowPlayingMovies= async()=>{
-     const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1',API_OPTIONS)
+    
+    //api call statement below
+     const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1',API_OPTIONS) 
+    //converts the returned promise into json
      const res= await data.json();
-     console.log(res?.results)
+     //dispatches the data(json) into the store (global state)
      dispatch(addNowPlayingMovie(res?.results)) 
   }
+useEffect(() => {
+  //called the api call method defined up above and it will be called for every render
+getNowPlayingMovies();
+}, [])
 
-  useEffect(()=>{
-   getNowPlayingMovies()
-  },[])
- 
-}
+};
 
-export default useNowPlayingMovies
+export default useNowPlayingMovies;
+
+
