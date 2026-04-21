1521d23709982705f941e2a088c859931cb331c8
Smarika-tech <smarikapokhrel96@gmail.com>
2026-04-21 15:50:45 +0545

    refactor:format files

--- src/hooks/useTopRatedMovies.jsx
+++ src/hooks/useTopRatedMovies.jsx
@@ -1,22 +1,22 @@
-import React, { useEffect } from 'react'
-import { API_OPTIONS } from '../utils/constant'
-import { useDispatch } from 'react-redux'
-import { addTopRatedMovies } from '../store/movieSlice';
+import React, { useEffect } from "react";
+import { API_OPTIONS } from "../utils/constant";
+import { useDispatch } from "react-redux";
+import { addTopRatedMovies } from "../store/movieSlice";
 
 const useTopRatedMovies = () => {
-  const dispatch= useDispatch();
-    const getTopRatedMovies=async()=>{
-        const data= await fetch('https://api.themoviedb.org/3/movie/top_rated?page=1', API_OPTIONS)
-        const res= await data.json();
-        console.log("res",res.results)
-        dispatch(addTopRatedMovies(res.results))
-    }
+  const dispatch = useDispatch();
+  const getTopRatedMovies = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/top_rated?page=1",
+      API_OPTIONS,
+    );
+    const res = await data.json();
+    dispatch(addTopRatedMovies(res.results));
+  };
 
-    useEffect(() => {
-      
-        getTopRatedMovies();
-    }, [])
-    
-}
+  useEffect(() => {
+    getTopRatedMovies();
+  }, []);
+};
 
-export default useTopRatedMovies
+export default useTopRatedMovies;
--- src/hooks/useUpcomingMovies.jsx
+++ src/hooks/useUpcomingMovies.jsx
@@ -1,22 +1,21 @@
-import React, { useEffect } from 'react'
-import { API_OPTIONS } from '../utils/constant'
-import { useDispatch } from 'react-redux'
-import { addUpComingMovies } from '../store/movieSlice';
+import React, { useEffect } from "react";
+import { API_OPTIONS } from "../utils/constant";
+import { useDispatch } from "react-redux";
+import { addUpComingMovies } from "../store/movieSlice";
 const useUpcomingMovies = () => {
- 
-  const dispatch= useDispatch();
-    const getUpComingMovies=async()=>{
-        const data= await fetch('https://api.themoviedb.org/3/movie/upcoming?page=1', API_OPTIONS)
-        const res= await data.json();
-        dispatch(addUpComingMovies(res?.results))
-    }
+  const dispatch = useDispatch();
+  const getUpComingMovies = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/upcoming?page=1",
+      API_OPTIONS,
+    );
+    const res = await data.json();
+    dispatch(addUpComingMovies(res?.results));
+  };
 
-    useEffect(() => {
-      console.log("running")
-        getUpComingMovies();
-    }, [])
-    
-}
+  useEffect(() => {
+    getUpComingMovies();
+  }, []);
+};
 
-
-export default useUpcomingMovies
+export default useUpcomingMovies;
